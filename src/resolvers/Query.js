c8d78f132f517dad612ca8c10edbc175665ed60c
Dmitry Levshin <Dmitry_Levshin@epam.com>
2019-05-12 16:14:21 +0300

    added new functions

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,1 +1,15 @@
-exports.feed = (parent, args, { prisma }) => prisma.links();
+exports.feed = async (parent, {filter, skip, first, orderBy}, {prisma}) => {
+    const where = filter
+        ? {
+              OR: [{description_contains: filter}, {url_contains: filter}],
+          }
+        : {};
+
+    const links = await prisma.links({where, skip, first, orderBy});
+    const count = await prisma
+        .linksConnection({where})
+        .aggregate()
+        .count();
+
+    return {links, count};
+};
